aaa337ff257094ee3c53fc746468f4d872510bb9
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-10-02 20:15:45 -0400

    Sugerencia

---
+++ DAL/Contexto.cs
@@ -0,0 +1,27 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Sugerencias> Sugerencias { get; set; }
+        public Contexto(): base("SugerenciaDB")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+
+    }
+}
---
+++ Entity/Sugerencias.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    [Serializable]
+    public class Sugerencias
+    {
+        [Key]
+        public int SugerenciaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public Sugerencias()
+        {
+            SugerenciaId = 0;
+            Fecha = DateTime.Now;
+            Descripcion = string.Empty;
+
+        }
+
+        public Sugerencias(int sugerenciaId, DateTime fecha, string descripcion)
+        {
+            SugerenciaId = sugerenciaId;
+            Fecha = fecha;
+            Descripcion = descripcion;
+        }
+    }
+}
